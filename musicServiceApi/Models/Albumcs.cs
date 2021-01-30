b4c5ec30a95f2e7c4f430bc2bba7f383b98db66f
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-01-30 18:55:28 -0500

    added mongo db driver and added base album class

---
+++ musicServiceApi/Models/Albumcs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace musicServiceApi.Models
+{
+    public class Albumcs
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        private String albumId;
+    }
+}
