8dd8111a413a9bd3d6895eadeceb9ded134d6c47
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-01-30 19:11:59 -0500

    added playlist model and added references to both album and song models

---
+++ musicServiceApi/Models/Playlist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace musicServiceApi.Models
+{
+    public class Playlist
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        private string playlistId;
+
+        [BsonElement("PlaylistName")]
+        [BsonRequired]
+        private string playlistName { get; set; }
+
+        [BsonElement("AlbumsInPlaylist")]
+        [BsonRequired]
+        private List<Album> albumsInPlaylist { get; set; }
+
+        [BsonElement("SongsInPlaylist")]
+        [BsonRequired]
+        private List<Song> songsInPlaylist { get; set; }
+
+        [BsonElement("PlaylistUser")]
+        [BsonRequired]
+        private string playlistUser { get; set; }
+
+        public Playlist()
+        {
+
+        }
+
+        public string PlaylistId { get => playlistId; set => playlistId = value; }
+
+
+    }
+}
