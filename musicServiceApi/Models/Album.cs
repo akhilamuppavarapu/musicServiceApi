b885523ce9c43dec1ca173f611d256e1f5b73967
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-01-30 19:00:04 -0500

    album attributes added

---
+++ musicServiceApi/Models/Album.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace musicServiceApi.Models
+{
+    public class Album
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        private String albumId;
+
+        [BsonElement("AlbumName")]
+        [BsonRequired]
+        private String albumName { get; set; }
+
+        [BsonElement("AlbumRelease")]
+        [BsonRequired]
+        private String albumRelease { get; set; }
+
+        [BsonElement("AlbumArtists")]
+        [BsonRequired]
+        private List<String> albumArtists { get; set; }
+
+
+        public Album()
+        {
+            // empty constructor
+        }
+      
+    }
+}
