e8cef888e687e9d80e38f68d109dc697a3beefb7
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-01-30 19:19:04 -0500

    added basic database layout with connection strings and song service class to perform crud operations

---
+++ musicServiceApi/MusicServiceApiDbSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace musicServiceApi
+{
+    public class MusicServiceApiDbSettings : IMusicServiceApiDbSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+    }
+
+    public interface IMusicServiceApiDbSettings
+    {
+        string ConnectionString { get; set; }
+
+        string DatabaseName { get; set; }
+    }
+
+}
---
+++ musicServiceApi/Services/SongService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using musicServiceApi.Models;
+
+namespace musicServiceApi.Services
+{
+    public class SongService
+    {
+        private readonly IMongoCollection<Song> songs;
+
+        public SongService(IMusicServiceApiDbSettings configuration)
+        {
+            MongoClient mongoClient = new MongoClient(configuration.ConnectionString);
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(configuration.DatabaseName);
+            songs = mongoDatabase.GetCollection<Song>("Song");
+        }
+
+        public List<Song> getSongs()
+        {
+            return songs.Find(song => true).ToList();
+        }
+
+        public Song getSong(string id)
+        {
+            return songs.Find<Song>(song => song.SongId == id).FirstOrDefault();
+        }
+
+        public Song addNewSong(Song song)
+        {
+            songs.InsertOne(song);
+            return song;
+        }
+
+    }
+}
