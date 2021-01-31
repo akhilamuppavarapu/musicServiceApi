5a63e67a185929210ecc5d61be37d7a4a6f9d5c2
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-01-30 19:21:35 -0500

    added album and playlist services for crud operations

---
+++ musicServiceApi/Services/AlbumService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using musicServiceApi.Models;
+
+namespace musicServiceApi.Services
+{
+    public class AlbumService
+    {
+        private readonly IMongoCollection<Album> albums;
+
+        public AlbumService(IMusicServiceApiDbSettings configuration)
+        {
+            MongoClient mongoClient = new MongoClient(configuration.ConnectionString);
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(configuration.DatabaseName);
+            albums = mongoDatabase.GetCollection<Album>("Album");
+        }
+
+        public List<Album> getAlbums()
+        {
+            return albums.Find(album => true).ToList();
+        }
+
+        public Album getAlbum(string id)
+        {
+            return albums.Find<Album>(album => album.AlbumId == id).FirstOrDefault();
+        }
+
+        public Album addNewAlbum(Album album)
+        {
+            albums.InsertOne(album);
+            return album;
+        }
+
+    }
+}
---
+++ musicServiceApi/Services/PlaylistService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using musicServiceApi.Models;
+
+namespace musicServiceApi.Services
+{
+    public class PlaylistService
+    {
+        private readonly IMongoCollection<Playlist> playlists;
+
+        public PlaylistService(IMusicServiceApiDbSettings configuration)
+        {
+            MongoClient mongoClient = new MongoClient(configuration.ConnectionString);
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(configuration.DatabaseName);
+            playlists = mongoDatabase.GetCollection<Playlist>("Playlist");
+        }
+
+        public List<Playlist> getPlaylists()
+        {
+            return playlists.Find(playlist => true).ToList();
+        }
+
+        public Playlist getPlaylist(string id)
+        {
+            return playlists.Find<Playlist>(playlist => playlist.PlaylistId == id).FirstOrDefault();
+        }
+
+        public Playlist addNewPlaylist(Playlist playlist)
+        {
+            playlists.InsertOne(playlist);
+            return playlist;
+        }
+
+    }
+}
